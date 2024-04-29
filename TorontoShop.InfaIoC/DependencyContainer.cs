89457977a1741fd087b2d7322100e3aa8f6fa9f5
98913 <98913@LAPTOP-JQR66TAF>
2024-04-29 12:33:57 +0330

    Add NugetPakeg EFCore

---
+++ TorontoShop.InfaIoC/DependencyContainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorontoShop.InfaIoC
+{
+    public class DependencyContainer
+    {
+        #region Services
+
+        #endregion
+
+
+        #region Reposirory
+
+        #endregion
+
+
+        #region Tools 
+
+        
+
+        #endregion
+    }
+}
