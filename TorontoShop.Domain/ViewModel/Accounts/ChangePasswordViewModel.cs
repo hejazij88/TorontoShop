38db756f672f2ac8e63f958fd85f10c525c14133
98913 <98913@LAPTOP-JQR66TAF>
2024-05-04 11:57:51 +0330

    Add ChengePassword VM

---
+++ TorontoShop.Domain/ViewModel/Accounts/ChangePasswordViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TorontoShop.Domain.ViewModel.Accounts
+{
+    public class ChangePasswordViewModel
+    {
+        [Display(Name = "کلمه ی عبور فعلی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string CurrentPassword { get; set; }
+
+        [Display(Name = "کلمه ی عبور جدید")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string NewPassword { get; set; }
+
+        [Display(Name = "تکرار کلمه ی عبور جدید")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        [Compare("NewPassword",ErrorMessage ="کلمه ی عبور جدید باهم مغایرت دارند")]
+        public string ConfirmNewPassword { get; set; }
+    }
+    public enum ChangePasswordResult
+    {
+        NotFound,
+        PasswordEqual,
+        Success
+    }
+}
