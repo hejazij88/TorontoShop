6ad68f9bcabd3ad182ebe9b7ac6a45ada8f27625
98913 <98913@LAPTOP-JQR66TAF>
2024-05-05 13:14:11 +0330

    Add Paging in wallet

---
+++ TorontoShop.Application/Extension/CommonExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorontoShop.Application.Extension
+{
+    public static class CommonExtension
+    {
+        public static string GetEnumName(this System.Enum dataEnum)
+        {
+            var enumDisplayName = dataEnum.GetType().GetMember(dataEnum.ToString()).FirstOrDefault();
+            if (enumDisplayName != null)
+            {
+                return enumDisplayName.GetCustomAttribute<DisplayAttribute>()?.GetName();
+            }
+            return "";
+        }
+    }
+}
