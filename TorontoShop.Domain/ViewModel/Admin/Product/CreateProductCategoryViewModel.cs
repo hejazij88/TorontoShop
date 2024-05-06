1cec6eb79981ccdc5ee1f0582a2e02fcfe5f15ad
98913 <98913@LAPTOP-JQR66TAF>
2024-05-06 15:00:46 +0330

    Add ProductCategory ViewModel

---
+++ TorontoShop.Domain/ViewModel/Admin/Product/CreateProductCategoryViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorontoShop.Domain.ViewModel.Admin.Product
+{
+    public class CreateProductCategoryViewModel
+    {
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string Title { get; set; }
+        [Display(Name = "آدرس")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(200, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد")]
+        public string UrlName { get; set; }
+    }
+
+    public enum CreateProductCategoryResult
+    {
+        [Display(Name = "موفق")]
+        Success,
+        [Display(Name = "تکراری")]
+        IsExist
+    }
+}
---
+++ TorontoShop.Domain/ViewModel/Admin/Product/ProductCategoryFilterViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TorontoShop.Domain.Model.Accounts;
+using TorontoShop.Domain.Model.ProductEntity;
+using TorontoShop.Domain.ViewModel.Admin.Account;
+using TorontoShop.Domain.ViewModel.Paging;
+
+namespace TorontoShop.Domain.ViewModel.Admin.Product
+{
+    public class ProductCategoryFilterViewModel:BasePaging
+    {
+        public string Title { get; set; }
+        public List<ProductCategory> Categories { get; set; }
+
+        public ProductCategoryFilterViewModel SetCategory(List<ProductCategory> categories)
+        {
+            this.Categories = categories;
+            return this;
+        }
+
+        public ProductCategoryFilterViewModel SetPaging(BasePaging paging)
+        {
+            this.PageId = paging.PageId;
+            this.AllEntityCount = paging.AllEntityCount;
+            this.StartPage = paging.StartPage;
+            this.EndPage = paging.EndPage;
+            this.TakeEntity = paging.TakeEntity;
+            this.CountForShowAfterAndBefor = paging.CountForShowAfterAndBefor;
+            this.SkipEntity = paging.SkipEntity;
+            this.PageCount = paging.PageCount;
+
+            return this;
+        }
+
+
+    }
+}
