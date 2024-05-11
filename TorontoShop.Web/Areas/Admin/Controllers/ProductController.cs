235985fe8313fd7abc5ecd1341a0575f85676f65
98913 <98913@LAPTOP-JQR66TAF>
2024-05-11 17:06:33 +0330

    Add Controller Create ProductCategory

--- TorontoShop.Web/Areas/Admin/Controllers/ProductController.cs
+++ TorontoShop.Web/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TorontoShop.Application.Interfaces;
+using TorontoShop.Domain.Model.ProductEntity;
 using TorontoShop.Domain.ViewModel.Admin.Product;
 
 namespace TorontoShop.Web.Areas.Admin.Controllers
@@ -21,6 +22,32 @@ namespace TorontoShop.Web.Areas.Admin.Controllers
         public async Task<IActionResult> FilterProductCategories(ProductCategoryFilterViewModel filter)
         {
             return View(await _productService.ProductCategoryFilter(filter));
+        }
+
+        [HttpGet]
+        public IActionResult CreateProductCategory()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateProductCategory(CreateProductCategoryViewModel productCategoryViewModel,
+            IFormFile image)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _productService.CreateProductCategory(productCategoryViewModel,image);
+                switch (result)
+                {
+                    case  CreateProductCategoryResult.IsExist:
+                        TempData[ErrorMessage] = "url تکراری است";
+                        break;
+                    case CreateProductCategoryResult.Success:
+                        TempData[SuccessMessage] = "با موفقیت ثبت شد";
+                        return RedirectToAction("FilterProductCategories");
+                }
+            }
+            return View(productCategoryViewModel);
         }
 
     }
