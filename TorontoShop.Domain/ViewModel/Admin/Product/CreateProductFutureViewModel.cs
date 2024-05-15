d687fa5f29dfc27316a9f326350399cc614731ed
98913 <98913@LAPTOP-JQR66TAF>
2024-05-15 11:12:02 +0330

    Add ViewModel CreateProduct Future

---
+++ TorontoShop.Domain/ViewModel/Admin/Product/CreateProductFutureViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TorontoShop.Domain.ViewModel.Admin.Product;
+
+public class CreateProductFutureViewModel
+{
+    public Guid ProductId { get; set; }
+    public string Title { get; set; }
+    public string Value { get; set; }
+}
+
+public enum CreateProductFutureResult
+{
+    Success
+    ,Error
+}
