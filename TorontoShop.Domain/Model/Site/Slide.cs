1965da6ffa4fd9b9ee8e3bd2ac1bab1ee926b94b
98913 <98913@LAPTOP-JQR66TAF>
2024-05-17 22:42:56 +0330

    Add Entity Slide

---
+++ TorontoShop.Domain/Model/Site/Slide.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TorontoShop.Domain.Model.BaseEntities;
+
+namespace TorontoShop.Domain.Model.Site
+{
+    public class Slide:BaseEntity
+    {
+        public string ImageName { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public int Price { get; set; }
+        public string Href { get; set; }
+        public string ButtonText { get; set; }
+    }
+}
